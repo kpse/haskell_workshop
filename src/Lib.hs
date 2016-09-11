5eff35ac6e30799ea91efadd4189d3b8b873bb38
Suo Qin <kpsesuo@gmail.com>
2016-09-11 11:49:36 +1000

    add test library Hspec

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,13 @@
 module Lib
-    ( 
-    	someFunc
-    ) 
+    (
+    	someFunc,
+      sum'
+    )
     where
 
 someFunc :: IO ()
 someFunc = putStrLn "This function is the best!"
+
+
+sum' :: (Num a) => a -> a -> a
+sum' a b = a + b
