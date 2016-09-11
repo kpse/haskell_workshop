1d0e35462e321504cdd08340ae91915e24a5e46b
Suo Qin <kpsesuo@gmail.com>
2016-09-11 12:00:29 +1000

    more tests in given describe

--- test/SumSpec.hs
+++ test/SumSpec.hs
@@ -4,5 +4,9 @@ import Lib
 
 import Test.Hspec
 
-spec = describe "Deal with 2 numbers" $
-    it "should sum 2" $ sum' 1 2 `shouldBe` 3
+spec = describe "Deal with 2 numbers" $ do
+    it "should sum 1 and 2" $
+      sum' 1 2 `shouldBe` 3
+
+    it "should sum 3 and 4" $
+      sum' 3 4 `shouldBe` 7
