5755b56fe0cfa77adc00ba1919cf7a34d652af4f
Louis Suo <louis.suo@myob.com>
2016-09-15 11:10:14 +1000

    correct spec description

--- test/MaximumSpec.hs
+++ test/MaximumSpec.hs
@@ -7,6 +7,8 @@ import Test.Hspec
 spec = describe "calculate the maximum number" $ do
     it "should give nothing if empty input" $ do
       maximum' ([] :: [Int]) `shouldBe` (Nothing :: Maybe Int)
+    it "should give out the the only one" $ do
       maximum' [1] `shouldBe` Just 1
+    it "should give out the maximum" $ do
       maximum' [1, 2] `shouldBe` Just 2
       maximum' [3, 1, 2] `shouldBe` Just 3
