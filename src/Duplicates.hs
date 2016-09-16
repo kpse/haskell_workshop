0d26e350ca27a68f4dba7c75ca3f89a963e633c6
Louis Suo <louis.suo@myob.com>
2016-09-16 17:10:26 +1000

    finish one kata, duplicates counting

---
+++ src/Duplicates.hs
@@ -0,0 +1,20 @@
+module Duplicates (
+	duplicateCount
+) where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Char (toLower)
+
+duplicateCount :: String -> Int
+duplicateCount [] = 0
+duplicateCount string = Map.size $ filterOut $ foldl createMap Map.empty string
+
+createMap :: Map Char Int -> Char -> Map Char Int
+createMap m c = let lowerC = toLower c in
+	case (Map.lookup lowerC m) of
+		Just x -> Map.insert lowerC (x+1) $ m
+		Nothing -> Map.insert lowerC 1 $ m
+
+filterOut :: Map Char Int -> Map Char Int
+filterOut m = Map.filter (>1) m
