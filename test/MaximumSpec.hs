17e8fc094ba6f86b879651de48aca24fd364a702
Louis Suo <louis.suo@myob.com>
2016-09-16 16:05:38 +1000

    second decribe example, two way to help type deduction

--- test/MaximumSpec.hs
+++ test/MaximumSpec.hs
@@ -5,10 +5,14 @@ import Lib
 import Test.Hspec
 
 spec = describe "calculate the maximum number" $ do
-    it "should give nothing if empty input" $ 
-      maximum' ([] :: [Int]) `shouldBe` (Nothing :: Maybe Int)
+  describe "first describe" $ do
+    it "should give nothing if empty input" $
+      maximum' ([]::[Int]) `shouldBe` Nothing
     it "should give out the maximum" $
       maximum' [1] `shouldBe` Just 1
     it "should give out the maximum" $ do
       maximum' [1, 2] `shouldBe` Just 2
       maximum' [3, 1, 2] `shouldBe` Just 3
+  describe "second describe" $
+    it "should give nothing if empty input" $
+      maximum' [] `shouldBe` (Nothing :: Maybe Int)
