16a51139e281481bed4e6c3375ff95d7693b5693
Suo Qin <kpsesuo@gmail.com>
2016-09-24 01:41:55 +1000

    fix width issue of UnlimitedGameOfLife

--- src/UnlimitedGameOfLife.hs
+++ src/UnlimitedGameOfLife.hs
@@ -1,30 +1,48 @@
 module UnlimitedGameOfLife where
 
 import Data.Function (on)
-import Data.List (groupBy, sort, splitAt)
+import Data.List (groupBy, sort, splitAt, transpose)
+-- import Debug.Trace
 
 type Pos = (Int, Int)
 
 getGeneration :: [[Int]] -> Int -> [[Int]]
 getGeneration world gen = case gen of
   0 -> world
-  n -> getGeneration (construct (length world) posInfo) (gen - 1)
-    where posInfo = [ ((row, col), nextRound (col,row) world) | row <-  [0 .. length world - 1], col <- [0 .. length (head world) - 1] ]
+  n -> getGeneration newWorld (gen - 1)
+    where
+      -- w = trace ("round n = " ++ show n ++ " new world = " ++ show newWorld ) newWorld
+      newWorld = construct (length (head expandedWorld)) posInfo
+      posInfo = [ ((row, col), nextRound (col,row) expandedWorld) | row <-  [0 .. length expandedWorld - 1], col <- [0 .. length (head expandedWorld) - 1] ]
+      expandedWorld = expand world
 
 nextRound :: Pos -> [[Int]] -> Int
-nextRound (x,y) world = let fate = sum $ map (\(a, b) -> world !! b !! a) $ neighbors (x, y) world in
-  case fate of
-    n | n < 2 || n > 3 -> 0
-    n | n == 3 && world !! y !! x == 0 -> 1
-    _ -> world !! y !! x
+nextRound (x,y) world = let fate = sum $ map (\(a, b) -> world !! b !! a) $ neighbors (x, y) world
+                            current = world !! y !! x
+                          in
+                          case fate of
+                            n | n < 2 || n > 3 -> 0
+                            n | n == 3 && current == 0 -> 1
+                            _ -> current
+
+expand :: [[Int]] -> [[Int]]
+expand [] = []
+expand world = transpose $ (transpose . transpose) [sideBar] ++ transpose ([topBar] ++ world ++ [topBar]) ++ [sideBar]
+  where topBar = [0 | _ <- [0 .. length (head world) - 1]]
+        sideBar = [0 | _ <- [0 .. length world + 1]]
+
+compact :: [[Int]] -> [[Int]]
+compact [] = [[]]
+compact a = (transpose . transpose . compact' .compact') a
+  where compact' = transpose . reverse . dropWhile (\w -> 0 == sum w) . reverse . dropWhile (\w -> 0 == sum w)
 
 neighbors :: Pos -> [[Int]] -> [Pos]
-neighbors (x, y) world = filter (\(a, b) -> onMap a b && (a,b)/=(x,y)) [(dx + x, dy + y) | dx <- [-1, 0, 1], dy <- [-1, 0, 1]]
+neighbors (x, y) world = filter  (\(a, b) -> onMap a b && (a,b)/=(x,y)) [(dx + x, dy + y) | dx <- [-1, 0, 1], dy <- [-1, 0, 1]]
   where onMap x y = x `elem` [0 .. length (head world) - 1] && y  `elem` [0 .. length world - 1]
 
 
 construct :: Int -> [(Pos, Int)] -> [[Int]]
-construct rowLength world = splitEvery rowLength [ life | (pos, life) <- sort world]
+construct rowLength world = compact $ splitEvery rowLength [ life | (pos, life) <- sort world]
 
 splitEvery _ [] = []
 splitEvery n list = first : splitEvery n rest
--- test/UnlimitedGameOfLifeSpec.hs
+++ test/UnlimitedGameOfLifeSpec.hs
@@ -12,6 +12,24 @@ start = [[1,0,0],
 end   = [[0,1,0],
          [0,0,1],
          [1,1,1]]
+end2  = [[1,0,1],
+         [0,1,1],
+         [0,1,0]]
+start3  = [[1,1,1,0,0,0,1,0],
+          [1,0,0,0,0,0,0,1],
+          [0,1,0,0,0,1,1,1]]
+end3  = [[1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1]
+         ]
 
 spec = describe ("Glider<br>" ++ htmlize start) $ do
   it "Glider 1" $ do
@@ -19,3 +37,13 @@ spec = describe ("Glider<br>" ++ htmlize start) $ do
         errorMsg = intercalate "<br>" ["expected:", htmlize end, "got:" , htmlize actual]
 
     assertBool errorMsg (actual == end)
+  it "Glider 2" $ do
+      let actual = getGeneration start 2
+          errorMsg = intercalate "<br>" ["expected:", htmlize end, "got:" , htmlize actual]
+
+      assertBool errorMsg (actual == end2)
+  it "Glider 100 " $ do
+      let actual = getGeneration start3 10
+          errorMsg = intercalate "<br>" ["expected:", htmlize end, "got:" , htmlize actual]
+
+      assertBool errorMsg (actual == end3)
