6cd4c05fb2ba7bd149dd82e5350df3c5cafb5a0f
Suo Qin <kpsesuo@gmail.com>
2016-09-23 23:45:07 +1000

    solution UnlimitedGameOfLife

--- src/UnlimitedGameOfLife.hs
+++ src/UnlimitedGameOfLife.hs
@@ -1,15 +1,35 @@
 module UnlimitedGameOfLife where
 
-type POS = (Int, Int)
-data LIVE = LIVE | DEAD
+import Data.Function (on)
+import Data.List (groupBy, sort, splitAt)
+
+type Pos = (Int, Int)
 
 getGeneration :: [[Int]] -> Int -> [[Int]]
-getGeneration input gen = [[0,1,0],
-         [0,0,1],
-         [1,1,1]]
+getGeneration world gen = case gen of
+  0 -> world
+  n -> getGeneration (construct (length world) posInfo) (gen - 1)
+    where posInfo = [ ((row, col), nextRound (col,row) world) | row <-  [0 .. length world - 1], col <- [0 .. length (head world) - 1] ]
 
-round :: POS -> [[Int]] -> Int
-round (x,y) world = 0
+nextRound :: Pos -> [[Int]] -> Int
+nextRound (x,y) world = let fate = sum $ map (\(a, b) -> world !! b !! a) $ neighbors (x, y) world in
+  case fate of
+    n | n < 2 || n > 3 -> 0
+    n | n == 3 && world !! y !! x == 0 -> 1
+    _ -> world !! y !! x
+
+neighbors :: Pos -> [[Int]] -> [Pos]
+neighbors (x, y) world = filter (\(a, b) -> onMap a b && (a,b)/=(x,y)) [(dx + x, dy + y) | dx <- [-1, 0, 1], dy <- [-1, 0, 1]]
+  where onMap x y = x `elem` [0 .. length (head world) - 1] && y  `elem` [0 .. length world - 1]
+
+
+construct :: Int -> [(Pos, Int)] -> [[Int]]
+construct rowLength world = splitEvery rowLength [ life | (pos, life) <- sort world]
+
+splitEvery _ [] = []
+splitEvery n list = first : splitEvery n rest
+  where
+    (first,rest) = splitAt n list
 
 htmlize :: [[Int]] -> String
 htmlize = concatMap ((++ "<br>") . concatMap cell)
