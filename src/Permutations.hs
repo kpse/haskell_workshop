905e7e52e6775c41c560e67b5456a38d277ae38d
Louis Suo <louis.suo@myob.com>
2016-09-23 11:49:44 +1000

    refactor permutations

--- src/Permutations.hs
+++ src/Permutations.hs
@@ -5,15 +5,12 @@ import Data.List (nub)
 
 permutations :: String -> [String]
 permutations "" = [""]
-permutations str = nub [ make str order | order <- gen2 (length str) []]
+permutations str = nub [ make str order | order <- generate (length str) []]
 
 make :: String -> [Int] -> String
 make str = map (str !!)
 
 generate :: Int -> [Int] -> [[Int]]
-generate n res = [ res ++ [x] | x <- [0..n-1], x `notElem` res]
-gen2 :: Int -> [[Int]] -> [[Int]]
-gen2 n [] = gen2 n $ generate n []
-gen2 n l = case l of
-  (x:xs) | length x == n -> l
-  _ -> gen2 n $ concatMap (generate n) l
+generate n res = case [ res ++ [x] | x <- [0..n-1], x `notElem` res] of
+    current@(x:xs) | length x == n -> current
+    current -> current >>= generate n
