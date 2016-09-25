fe6ebb661a2da4e48814dc33ba7827bc6e4c6119
Suo Qin <kpsesuo@gmail.com>
2016-09-25 22:17:00 +1000

    solution Fibonacci

---
+++ src/Fibonacci.hs
@@ -0,0 +1,18 @@
+module Fibonacci where
+
+import Debug.Trace
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib (-1) = 1
+fib (-2) = -1
+fib n | n < 0 = fib' (-1) 1 2 (n + 3)
+    where fib' pre pre2 result count = case count of
+                                        x | x < 0 -> fib' result pre (pre - result) (x + 1)
+                                        _ -> result
+
+fib n = fib' 1 0 0 (n - 1)
+   where fib' pre pre2 result count = case count of
+                                       x | x > 0 -> fib' (pre + pre2) pre (pre + pre2) (x - 1)
+                                       _ -> result
