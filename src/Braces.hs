d3b51bf5aad6a09ec8b226698e97c55963345ff1
Suo Qin <kpsesuo@gmail.com>
2016-09-26 22:57:24 +1000

    rename method

--- src/Braces.hs
+++ src/Braces.hs
@@ -7,14 +7,14 @@ validBraces [] = True
 validBraces input = case head input of
   open | open `elem` allOpenBrackets ->
     case closeAndAfter of
-      (closeBracket:rest) | closeBrackets open == Just closeBracket ->
+      (closeBracket:rest) | closeBracketFor open == Just closeBracket ->
         validBraces inBrackets && validBraces rest
       _ -> False
-    where (inBrackets,closeAndAfter) = span ((/= closeBrackets open) . Just) $ tail input
+    where (inBrackets,closeAndAfter) = span ((/= closeBracketFor open) . Just) $ tail input
   _ -> False
 
-closeBrackets :: Char -> Maybe Char
-closeBrackets c = find ((c ==) . head) brackets >>= Just . last
+closeBracketFor :: Char -> Maybe Char
+closeBracketFor c = find ((c ==) . head) brackets >>= Just . last
 
 brackets = ["()", "[]", "{}"]
 allOpenBrackets = map head brackets
