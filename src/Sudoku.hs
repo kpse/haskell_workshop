4406b6cc9a3d7b3f039c4cd9bb0f053434ecb241
Suo Qin <kpsesuo@gmail.com>
2016-10-02 01:46:21 +1000

    speed up sudoku by more smart generating

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -12,38 +12,32 @@ sudoku source = fromMaybe [] res
     res = find correct $ randomFill source
 
 correct :: Table -> Bool
-correct source = allEqual countPerLine && allEqual conunPerRow && subTableCorrect source
+correct source = allEqual countPerLine && allEqual conunPerRow
   where
     allEqual line = maximum line == minimum line
     countPerLine = map sum source
     conunPerRow = map sum $ transpose source
 
-slice :: Int -> Int -> [[Int]] -> [[Int]]
-slice from to xs = take (to - from + 1) (drop from xs)
-
-subTableCorrect :: [[Int]] -> Bool
-subTableCorrect [] = True
-subTableCorrect table = sub1 && sub2 && sub3
-  where
-    sub1 = 9 == (length . nub) (concatMap (take 3) $ take 3 table)
-    sub2 = 9 == (length . nub) (concatMap (take 3 . drop 3) $ (take 3 . drop 3) table)
-    sub3 = 9 == (length . nub) (concatMap (take 3 . drop 6) $ (take 3 . drop 6) table)
-
-
 randomFill :: Table -> [Table]
 randomFill table = case other of
   [] -> [table]
-  (zero:rest) -> concatMap randomFill [start ++ [x] ++ rest | x <- lineReplace table zero]
+  (thieLine:rest) -> concatMap randomFill [start ++ [x] ++ rest | x <- lineReplace table row thieLine]
   where
-    row = transpose table !! length start
+    row = length start
     (start, other) = span (notElem 0) table
 
-
-lineReplace :: Table -> Line -> [Line]
-lineReplace table line = case other of
-  [] -> [line]
-  (zero:rest) -> concatMap (lineReplace table) [ start ++ [x] ++ rest | x <- pick]
+subTableOf :: Table -> Int -> Int -> Line
+subTableOf table x y = concatMap (take 3 . drop (3 * xIndex)) $ (take 3 . drop (3 * yIndex)) table
   where
-    pick = [ x | x <- [1..9], x `notElem` line, x `notElem` row]
-    row = transpose table !! length start
+    xIndex = x `div` 3
+    yIndex = y `div` 3
+
+lineReplace :: Table -> Int -> Line -> [Line]
+lineReplace table row line = case other of
+  [] -> [line]
+  (zero:rest) -> concatMap (lineReplace table row) [ start ++ [x] ++ rest | x <- pick]
+  where
+    pick = [ x | x <- [1..9], x `notElem` line, x `notElem` chosen, x `notElem` col]
+    chosen = subTableOf table (length start) row
+    col = transpose table !! length start
     (start, other) = span (/=0) line
