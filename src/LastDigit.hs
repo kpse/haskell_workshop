0bd9d615f52febcc3f995d7e2cf41de4b9c9a6e7
Suo Qin <kpsesuo@gmail.com>
2016-10-04 17:32:44 +1100

    more test for solution last digit

--- src/LastDigit.hs
+++ src/LastDigit.hs
@@ -7,11 +7,12 @@ lastDigit [] = 1
 lastDigit [x] = x `mod` 10
 lastDigit [x,0] = 1
 lastDigit (1:xs) = lastDigit []
-lastDigit (x:arr) | 0 `elem` arr = case p of
-    0 -> lastDigit [x,0]
-    a -> lastDigit [x,a]
-  where
-    p = foldr (^) 1 $ remove00 arr
+lastDigit (0:arr) | 0 `elem` arr = case lastDigit arr of
+    0 -> lastDigit [0, head arr]
+    x -> 0
+
+lastDigit (x:arr) | 0 `elem` arr = let p = foldr (^) 1 $ remove00 arr in lastDigit [x,p]
+
 lastDigit arr = lastDigit' (arr ++ [1])
 
 lastDigit' :: [Integer] -> Integer
@@ -23,7 +24,13 @@ lastDigit' (x:p:xs) | p `mod` 4 == 3 = x ^ 3 `mod` 10
 lastDigit' (x:p:xs) | p `mod` 4 == 0 = x ^ 4 `mod` 10
 
 remove00 :: [Integer] -> [Integer]
-remove00 arr | [0,0] `isInfixOf` arr = (reverse .replace0. reverse) arr
+remove00 [0] = [0]
+remove00 arr | [0,0] `isInfixOf` arr = remove00 $ (reverse .replace0. reverse) arr
+remove00 arr | 0 `elem` arr = remove00 $ case rest of
+    [] -> [0]
+    some -> init some
+  where
+    rest = takeWhile (/=0) arr
 remove00 arr = replace0 arr
 
 replace0 :: [Integer] -> [Integer]
--- test/LastDigitSpec.hs
+++ test/LastDigitSpec.hs
@@ -10,17 +10,22 @@ examples = do
     lastDigit [0,0,0]    `shouldBe` 0 -- 0^(0 ^ 0) = 0^1 = 0
     lastDigit [0,0,1]    `shouldBe` 1
     lastDigit [2,0,0]    `shouldBe` 2
+    lastDigit [2,0,1]    `shouldBe` 1
+  it "should work for some examples 2" $ do
     lastDigit [0,0,2]    `shouldBe` 1
     lastDigit [2,2,0]    `shouldBe` 2
     lastDigit [10,0,0]    `shouldBe` 0
     lastDigit [0,10,1,0]    `shouldBe` 0
+    lastDigit [0,10,1,0,0]    `shouldBe` 0
     lastDigit [7,10,0,0]    `shouldBe` 9
     lastDigit [2,10,1,0]    `shouldBe` 4
+  it "should work for some examples 3" $ do
     lastDigit [1,2]      `shouldBe` 1
     lastDigit [3,4,5]    `shouldBe` 1
     lastDigit [4,3,6]    `shouldBe` 4
     lastDigit [7,6,21]   `shouldBe` 1
     lastDigit [12,30,21] `shouldBe` 6
+  it "should work for some examples 4" $ do
     lastDigit [937640,767456,981242] `shouldBe` 0
     lastDigit [123232,694022,140249] `shouldBe` 6
     lastDigit [499942,898102,846073] `shouldBe` 6
