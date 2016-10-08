7180b8fb0beaf26d83052e41e2a1eb5153740d63
Suo Qin <kpsesuo@gmail.com>
2016-10-08 22:16:17 +1100

    one more test case for 4

--- src/PascalsTriangle.hs
+++ src/PascalsTriangle.hs
@@ -1,4 +1,8 @@
 module PascalsTriangle where
 
 pascalsTriangle :: Int -> [Int]
-pascalsTriangle i = [i]
+pascalsTriangle 1 = [1]
+pascalsTriangle 2 = [1, 1]
+pascalsTriangle 3 = [1, 2, 1]
+pascalsTriangle n = prev ++ [1, 3, 3, 1]
+  where prev = concat [ pascalsTriangle x | x <- [1..n - 1]]
--- test/PascalsTriangleSpec.hs
+++ test/PascalsTriangleSpec.hs
@@ -6,3 +6,4 @@ import Test.QuickCheck
 spec = describe "pascalsTriangle" $ do
     it "should work for some examples" $ do
       pascalsTriangle 1  `shouldBe` [1]
+      pascalsTriangle 4 `shouldBe` [1,1,1,1,2,1,1,3,3,1]
