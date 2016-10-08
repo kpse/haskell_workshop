00972114c921a983e162742ef8feb785bc845d64
Suo Qin <kpsesuo@gmail.com>
2016-10-08 22:45:01 +1100

    solution pascals triangle

--- src/PascalsTriangle.hs
+++ src/PascalsTriangle.hs
@@ -1,8 +1,10 @@
 module PascalsTriangle where
 
 pascalsTriangle :: Int -> [Int]
-pascalsTriangle 1 = [1]
-pascalsTriangle 2 = [1, 1]
-pascalsTriangle 3 = [1, 2, 1]
-pascalsTriangle n = prev ++ [1, 3, 3, 1]
-  where prev = concat [ pascalsTriangle x | x <- [1..n - 1]]
+pascalsTriangle n = concat [ pascalsTriangle' x | x <- [1..n]]
+
+pascalsTriangle' :: Int -> [Int]
+pascalsTriangle' 1 = [1]
+pascalsTriangle' n = [1] ++ zipWith (+) lastLine (tail lastLine) ++ [1]
+  where
+    lastLine = pascalsTriangle' (n-1)
