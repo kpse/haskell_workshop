e899a960e3538e1729590efd7de82837fc50300e
Suo Qin <kpsesuo@gmail.com>
2016-10-18 22:24:39 +1100

    add more test suite to last digit

--- test/LastDigitSpec.hs
+++ test/LastDigitSpec.hs
@@ -13,6 +13,8 @@ examples = do
     lastDigit [2,0,1]    `shouldBe` 1
     lastDigit [2,0,0,1]  `shouldBe` 2
     lastDigit [0,0,1,0]  `shouldBe` 1
+    lastDigit [1,0,0]    `shouldBe` 1
+    lastDigit [0,0,1]    `shouldBe` 1
   it "should work for some examples 2" $ do
     lastDigit [0,0,2]    `shouldBe` 1
     lastDigit [2,2,0]    `shouldBe` 2
@@ -39,6 +41,14 @@ simpleProperties = do
     property (\(NonNegative x) -> lastDigit [x] `shouldBe` x `mod` 10)
   it "lastDigit [x, y] == x ^ y `mod` 10" $
     property (\(NonNegative x) (NonNegative y) -> lastDigit [x, y] `shouldBe` x ^ y `mod` 10)
+  it "lastDigit [1, y, z] == 1 ^ y ^ z `mod` 10" $
+    property (\(NonNegative x) (NonNegative y) -> lastDigit [1, x, y] `shouldBe` 1 ^ x ^ y `mod` 10)
+  it "lastDigit [0, y, z] == 0 ^ y ^ z `mod` 10" $
+    property (\(NonNegative x) (NonNegative y) -> lastDigit [0, x, y] `shouldBe` 0 ^ x ^ y `mod` 10)
+  it "lastDigit [x, 0, y] == x ^ 0 ^ y `mod` 10" $
+    property (\(NonNegative x) (NonNegative y) -> lastDigit [x, 0, y] `shouldBe` x ^ 0 ^ y `mod` 10)
+  it "lastDigit [0, y, z] == 0 ^ y ^ z `mod` 10" $
+    property (\(NonNegative x) (NonNegative y) -> lastDigit [x, y, 0] `shouldBe` x ^ y ^ 0 `mod` 10)
 
 spec = describe "main" $ do
   describe "Examples" examples
